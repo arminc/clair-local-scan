b52887cdb2d62ac7656dcdc98250454195d6b5ea
Alan Paruszewski <mparuszewski@gitlab.com>
2021-01-18 17:01:47 +0100

    Add terminating on error
    This change reverts checking for "error" text in logs and moves checking
    for error to first place to prevent from having false positives: having
    created database without all sources.

--- check.sh
+++ check.sh
@@ -4,15 +4,15 @@ CONTAINER="${1:-clair}"
 
 while true
 do
-    docker logs "$CONTAINER" | grep "libvuln initialized" >& /dev/null
-    if [ $? == 0 ]; then
-        break
-    fi
-
-    docker logs "$CONTAINER" | grep "updating errors" >& /dev/null
+    docker logs "$CONTAINER" | grep "error" >& /dev/null
     if [ $? == 0 ]; then
         echo "Error during update." >&2
         exit 1
+    fi
+
+    docker logs "$CONTAINER" | grep "libvuln initialized" >& /dev/null
+    if [ $? == 0 ]; then
+        break
     fi
 
     docker logs "$CONTAINER" | grep "warning" >& /dev/null
