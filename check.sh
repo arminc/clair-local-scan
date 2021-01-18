155f647d66b626a4ab96fc68a4bea62348ecc2cb
Alan Paruszewski <mparuszewski@gitlab.com>
2021-01-18 07:29:33 +0100

    Update clair to 4.0.0-rc24

--- check.sh
+++ check.sh
@@ -4,17 +4,17 @@ CONTAINER="${1:-clair}"
 
 while true
 do
-    docker logs "$CONTAINER" | grep "update finished" >& /dev/null
+    docker logs "$CONTAINER" | grep "libvuln initialized" >& /dev/null
     if [ $? == 0 ]; then
         break
     fi
 
-    docker logs "$CONTAINER" | grep "error" >& /dev/null
+    docker logs "$CONTAINER" | grep "updating errors" >& /dev/null
     if [ $? == 0 ]; then
         echo "Error during update." >&2
         exit 1
     fi
-    
+
     docker logs "$CONTAINER" | grep "warning" >& /dev/null
     if [ $? == 0 ]; then
         echo "Warning during update." >&2
